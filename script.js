13eb956d374b6e54915f66cb4a8cf7ff77397d3b
hugo <hugo.lagauzere@etu.unicaen.fr>
2023-09-11 15:03:36 +0200

    boucle timer

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ let timer=document.getElementById("timer");
 
 reset.style.display='none';
 
-let travail=25;
-let pause= 5;
+let travail=0.2;
+let pause= 0.1;
 
 // timer.textContent=travail+":"+sec;
 
-let bolean=0;
+let boolean=false;
 
-function passerTemps(x,y){
+function passerTemps(x,){
+
     let temps=x*60;
     
   let timerid=  setInterval(() => {
@@ -30,8 +31,6 @@ function passerTemps(x,y){
         timer.textContent = `${minutes}:${secondes}`
         temps = temps <= 0 ? 0 : temps - 1
       }, 1000)
-      play.style.display='none';
-      reset.style.display='block';
 }
 
 
@@ -40,5 +39,17 @@ reset.addEventListener("click",()=>{
 });
 
 play.addEventListener("click",()=>{
-    passerTemps(travail);
+    play.style.display='none';
+    reset.style.display='block';
+    
+
+    if (boolean==false){
+        passerTemps(travail);
+        boolean = true;
+    }
+    else if(boolean==true){
+        passerTemps(pause);
+    
+}
+    
 });
