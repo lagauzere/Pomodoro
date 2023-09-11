bd2f38dfd0bb89a39fdaa6c4f9aef17134641124
hugo <hugo.lagauzere@etu.unicaen.fr>
2023-09-11 14:13:50 +0200

    timer simple

--- script.js
+++ script.js
@@ -5,12 +5,30 @@ let timer=document.getElementById("timer");
 let travail=25;
 let pause= 5;
 
-timer.textContent=travail;
+// timer.textContent=travail+":"+sec;
+
 
 
 
 button.addEventListener("click",()=>{
-    alert("click");
+    let temps=travail*60;
+    
+    setInterval(() => {
+        let minutes = parseInt(temps / 60, 10)
+        let secondes = parseInt(temps % 60, 10)
+      
+        // minutes = minutes < 10 ? "0" + minutes : minutes
+        if (minutes<10){
+            minutes="0"+minutes;
+        }
+        // secondes = secondes < 10 ? "0" + secondes : secondes
+        if(secondes<10){
+            secondes="0"+secondes;
+        }
+      
+        timer.textContent = `${minutes}:${secondes}`
+        temps = temps <= 0 ? 0 : temps - 1
+      }, 1000)
 
 
 });
