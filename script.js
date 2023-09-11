d6a24f92a94c4ac48b24e8da6018f863eaffa810
hugo <hugo.lagauzere@etu.unicaen.fr>
2023-09-11 13:43:15 +0200

    ajout css

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let button= document.getElementById("play");
 let timer=document.getElementById("timer");
 
 
+let travail=25;
+let pause= 5;
+
+timer.textContent=travail;
+
 
 
 button.addEventListener("click",()=>{
